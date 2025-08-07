221926dd32d2c4eda8f090b39a02c1c5bbaa0750
Wasatch Wise <admin@wasatchwise.com>
2025-08-07 18:48:50 +0000

    Add simple test API endpoint

--- api/test-simple.ts
+++ api/test-simple.ts
@@ -1,6 +1,8 @@
-export default function handler(req: any, res: any) {
-  res.json({
-    message: "Simple API test",
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    message: 'Simple test API working!',
     timestamp: new Date().toISOString(),
     method: req.method,
     url: req.url
